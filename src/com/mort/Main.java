4ca70d7281bc7c4a1989b411bb1206e0fd465dfa
damosman <s.akinboboye@gmail.com>
2022-09-24 13:50:54 +0200

    Mortgage Calculator starting commit

---
+++ src/com/mort/Main.java
@@ -0,0 +1,12 @@
+package com.mort;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner Principal = new Scanner(System.in);
+        System.out.println("Principal: ");
+        Double principal = Principal.nextDouble();
+        System.out.println(principal);
+    }
+}
